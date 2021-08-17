2399fd192ef9e4125f9c64550c4b9525d140cd08
JuliRaineri <julietaraineri@gmail.com>
2021-08-17 13:40:41 -0300

    some files updated

--- poke-handler/script.js
+++ poke-handler/script.js
@@ -18,15 +18,38 @@ dataApi.requestRandomPokemon().then((pokemon) => {
 
    const pokeId = document.querySelector('#pokemon-id') ;
    pokeId.innerHTML = pokemon.id ;
-   const pokeType = document.querySelector('.pokemon-name');
+
+   const pokeType = document.querySelector('.poke-type');
    pokeType.innerHTML = pokemon.type[0] ;
-   const pokeName = document.querySelector('.poke-type');
-   pokeName.innerHTML = pokemon.name.english ;
+
    const pokeImg = document.querySelector('#pokemon-img') ;
    pokeImg.src = pokemon.thumbnail ;
+   
+   const pokeName = document.querySelector('.pokemon-name');
+   pokeName.innerHTML = pokemon.name.english ;
+
+   const pokeNameFr = document.querySelector('.pokemon-name-fr');
+   pokeNameFr.innerHTML = pokemon.name.french ;
+
+   const pokeNameChn = document.querySelector('.pokemon-name-chn');
+   pokeNameChn.innerHTML = pokemon.name.chinese ;
+
+   const pokeNameJpn = document.querySelector('.pokemon-name-jpn');
+   pokeNameJpn.innerHTML = pokemon.name.japanese ;
+
+   const pokeProfile = document.querySelector('.pokemon-profile');
+   pokeProfile.innerHTML = pokemon.species ;
+
+   const pokeAbility = document.querySelector('.pokemon-ability');
+   pokeAbility.innerHTML = pokemon.profile.ability[0] ;
+
+  
    const pokeStats = document.querySelector('.poke-stats') ;
    pokeStats.innerHTML = pokemon.base.Attack;
    
+   const pokeDescription = document.querySelector('.poke-description') ;
+   pokeDescription.innerHTML = pokemon.description;
+
 
 })
 
